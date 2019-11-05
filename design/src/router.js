fd64050b79a185d78c11b88d78a75e63c81dbe5e
齐丹萍 <2013668162@qq.com>
2019-11-05 19:23:38 +0800

    qdp

--- design/src/router.js
+++ design/src/router.js
@@ -7,7 +7,8 @@ const Login=loadable(()=>import('./pages/login/login'))
 const Home=loadable(()=>import('./pages/home/home'))
 const Admin=loadable(()=>import('./pages/admin/admin'))
 const Category=loadable(()=>import('./pages/category/category'))
-
+const AddCategory=loadable(()=>import('./pages/category/addCategory'))
+// const AddCategory=loadable(()=>import('./pages/category/addCategory'))
 
 class RootRoute extends React.Component{
     render(){
@@ -21,6 +22,7 @@ class RootRoute extends React.Component{
                             <Admin>
                                 <Route path='/admin/home' component={Home}></Route>
                                 <Route path='/admin/category' component={Category}></Route>
+                                <Route path='/admin/addCategory' component={AddCategory}></Route>
                             </Admin>
                         )
                     }}></Route>
